c0a1ad5bd946cfcdcbfbfd5ac2cf4d5a0301e658
Lens Net <lens.net@ocularedf.local>
2022-09-09 11:27:34 -0300

    fix: update agreements filter

--- dist/data/filteredAgreements.js
+++ dist/data/filteredAgreements.js
@@ -4,51 +4,51 @@ exports.filteredAgreements = void 0;
 exports.filteredAgreements = [
     {
         id: 48,
-        agreements: ["AMIL", "BOMBEIROS", "CASSI", "LIFE EMPRESARIAL", "MEDSENIOR", "PREVEN MED", "SAUDE SIN PG NO ATO", "SESC PG NO ATO", "VIDA CARD/ASSEDF", "REEMBOLSO AMIL"]
+        agreements: ["AMIL", "BOMBEIROS", "CASSI", "LIFE EMPRESARIAL", "MEDSENIOR", "PREVEN MED", "SAUDE SIN PG NO ATO", "SESC PG NO ATO", "VIDA CARD/ASSEDF"]
     },
     {
         id: 50,
-        agreements: ["CASSI", "AMIL", "REEMBOLSO AMIL"]
+        agreements: ["CASSI", "AMIL"]
     },
     {
         id: 53,
-        agreements: ["AMIL", "REEMBOLSO AMIL"]
+        agreements: ["AMIL"]
     },
     {
         id: 91,
-        agreements: ["AMIL", "REEMBOLSO AMIL"]
+        agreements: ["AMIL"]
     },
     {
         id: 84,
-        agreements: ["AMIL", "REEMBOLSO AMIL"]
+        agreements: ["AMIL"]
     },
     {
         id: 46,
-        agreements: ["AMIL", "REEMBOLSO AMIL"]
+        agreements: ["AMIL"]
     },
     {
         id: 49,
-        agreements: ["AMIL", "MEDSENIOR", "REEMBOLSO AMIL"]
+        agreements: ["AMIL", "MEDSENIOR"]
     },
     {
         id: 47,
-        agreements: ["AMIL", "MEDSENIOR", "REEMBOLSO AMIL"]
+        agreements: ["AMIL", "MEDSENIOR"]
     },
     {
         id: 71,
-        agreements: ["AMIL", "ASFUB", "SESC PG NO ATO", "REEMBOLSO AMIL"]
+        agreements: ["AMIL", "ASFUB", "SESC PG NO ATO"]
     },
     {
         id: 93,
-        agreements: ["AMIL", "REEMBOLSO AMIL"]
+        agreements: ["AMIL"]
     },
     {
         id: 89,
-        agreements: ["AMIL", "REEMBOLSO AMIL"]
+        agreements: ["AMIL"]
     },
     {
         id: 92,
-        agreements: ["AMIL", "MEDSENIOR", "SMILE SAUDE", "REEMBOLSO AMIL"]
+        agreements: ["AMIL", "MEDSENIOR", "SMILE SAUDE"]
     },
 ];
 //# sourceMappingURL=filteredAgreements.js.map
